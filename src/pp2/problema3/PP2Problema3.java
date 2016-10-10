cc33e83a503ba736d5799092ebe4ef2575490131
PC <PC@HP>
2016-10-10 18:25:30 -0500

    3.	Realizar un programa que calcule el factorial de un número dado.

--- src/pp2/problema3/PP2Problema3.java
+++ src/pp2/problema3/PP2Problema3.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema3;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,20 @@ public class PP2Problema3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       gerge
+       
+    }
+    public static int solicitarDatos(String d){ //Solicita datos
+        int miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextInt();
+        return miDato;
+    }
+    public static int encontrarFactorial(){//s
+        int a = solicitarDatos("numero para calcular el factorial");
+        
+            
+       
     }
     
 }
